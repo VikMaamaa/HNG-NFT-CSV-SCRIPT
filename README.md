4be6793d303ebe09e9a1528b41684d748baa7323
vikmaamaa <maamaavictor@yahoo.com>
2022-11-03 15:26:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# NFT CSV SCRIPT
+
+
+
+
+
+
+NFT CSV Script is a script use to generate Chip 7 JSON files and calculate sha256 for each JSON file and appends to your CSV file.
+
+- Clone the repository at https://github.com/VikMaamaa/HNG-NFT-CSV-SCRIPT
+- Ensure you have a package manager like Npm or Yarn installed on your computer.
+- Copy your csv File into the folder.
+- Open your terminal.
+- On your terminal ensure, you are in the folder, where the script is located.
+- Enter the command ‘npm install’ or ‘yarn install’.
+```sh
+npm install
+or
+yarn install
+```
+- Type in the command ‘npm start’ or ‘yarn start’
+```sh
+npm start
+or
+yarn start
+```
+- At the filename prompt on your terminal enter the filename 
+> Note: `Do not enter the file format, enter only the filename, e.g if the file is ‘james.csv’, enter ‘james’ .`
+
+- Your csv file can be found with as orginalFilename.output.csv
+> Note: `e.g if the file is ‘james.csv’, your output file can be found as  ‘james.output.csv’ `
+- Your generated json files can be found in the ‘files’ folder.
+- ✨Magic ✨
+
---
+++ index.js
@@ -0,0 +1,138 @@
+const fs = require('fs')
+const csv = require('fast-csv');
+const crypto = require('crypto');
+const { Parser } = require('json2csv');
+
+
+
+
+
+const data = []
+const records = [];
+const dats = []
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  readline.question(`Enter Filename (do not include the extension)?  `, name => {
+    console.log(`File Name: ${name}.csv`);
+  
+  
+    fs.createReadStream(`./${name}.csv`)
+  .pipe(csv.parse({ headers: true }))
+  .on("headers", (headers) => {
+    headers.push("Hash");
+
+
+
+    const headerRow = {};
+    headers.forEach((col) => {
+      headerRow[col] = col;
+    });
+
+    headerRow["Hash"] = "Hash";
+    records.push(headerRow);
+
+
+   
+  })
+  .on('error', error => console.error(error))
+  .on('data', row =>{
+    row.Hash = crypto.createHash('sha256').update(JSON.stringify(row)).digest('hex')
+    data.push(row)
+    const ser = row["Series Number"] || "";
+    if (ser.toLowerCase().startsWith("team")) {
+      cur = ser;
+    }
+
+    if (row["Filename"]) {
+      dats.push({ ...row, Team: cur });
+      records.push(row);
+    } else {
+      records.push(row);
+    }
+  })
+  .on('end', () => {
+    // console.log(data)
+   
+    
+    dats.forEach((dat) => {
+      const jsonData = {
+        format: "CHIP-0007",
+        name: dat["Name"],
+        description: dat["Description"],
+        minting_tool: dat["Team"],
+        sensitive_content: false,
+        series_number: parseInt(dat["Series Number"]),
+        series_total: dats.length,
+        attributes: [
+          {
+            trait_type: "gender",
+            value: dat["Gender"],
+          },
+        ],
+        collection: {
+          name: "Zuri dat Tickets for Free Lunch",
+          id: "b774f676-c1d5-422e-beed-00ef5510c64d",
+          attributes: [
+            {
+              type: "description",
+              value: "Rewards for accomplishments during HNGi9.",
+            },
+          ],
+        },
+      };
+
+      // Add more attributes field if available
+      if (dat["Attributes"]) {
+        dat["Attributes"].split(",").forEach((attribute) => {
+          if (attribute) {
+            try {
+              const values = attribute.split(":");
+              const traitType = values[0].trim();
+              const value = values[1].trim();
+
+              jsonData["attributes"].push({
+                trait_type: traitType,
+                value: value,
+              });
+            } catch (err) {
+              console.log("Invalid attribute in the file, please rectify it: ", attribute);
+            }
+          }
+        });
+      }
+
+ 
+
+   
+      const hashed = crypto.createHash('sha256').update(JSON.stringify(jsonData)).digest('hex')
+      jsonData["Hash"] = hashed;
+    
+      fs.writeFileSync(`./files/${dat["Filename"]}.json`, JSON.stringify(jsonData));
+
+     
+    
+    });
+
+    // Write the CSV
+    try {
+        const parser = new Parser();
+        const csv = parser.parse(data);
+        fs.appendFile(`${name}.output.csv`, csv, 'utf-8', err => {
+            if (err) {
+              throw err;
+            }
+        })
+        // console.log(csv);
+      } catch (err) {
+        console.error(err);
+      }
+})
+
+
+// console.log(data)
+    readline.close();
+  })
